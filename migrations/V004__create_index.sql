525891e360079df97b3043b68dfa9e1ae8f80816
mifimov345 <mifimov345@gmail.com>
2026-03-12 19:54:24 +0300

    third commit

---
+++ migrations/V004__create_index.sql
@@ -0,0 +1,11 @@
+CREATE INDEX idx_orders_status
+ON orders(status);
+
+CREATE INDEX idx_orders_date_created
+ON orders(date_created);
+
+CREATE INDEX idx_order_product_order_id
+ON order_product(order_id);
+
+CREATE INDEX idx_order_product_product_id
+ON order_product(product_id);

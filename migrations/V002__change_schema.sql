9e55ffa58e5e4ceb0280a6f18f5279f0d1f2935f
mifimov345 <mifimov345@gmail.com>
2026-03-12 19:47:03 +0300

    second commit

---
+++ migrations/V002__change_schema.sql
@@ -0,0 +1,33 @@
+ALTER TABLE product
+ADD COLUMN price DOUBLE PRECISION;
+
+UPDATE product p
+SET price = pi.price
+FROM product_info pi
+WHERE p.id = pi.product_id;
+
+ALTER TABLE orders
+ADD COLUMN date_created DATE DEFAULT CURRENT_DATE;
+
+UPDATE orders o
+SET date_created = od.date_created
+FROM orders_date od
+WHERE o.id = od.order_id;
+
+DROP TABLE product_info;
+
+DROP TABLE orders_date;
+
+ALTER TABLE product
+ADD CONSTRAINT product_pkey PRIMARY KEY (id);
+
+ALTER TABLE orders
+ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+ALTER TABLE order_product
+ADD CONSTRAINT fk_order
+FOREIGN KEY (order_id) REFERENCES orders(id);
+
+ALTER TABLE order_product
+ADD CONSTRAINT fk_product
+FOREIGN KEY (product_id) REFERENCES product(id);
